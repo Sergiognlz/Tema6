8b2aae734c3828cc4d4d9f819b63ef1c8f8baea4
sgonzalez <sgonzalez@106-03.info2>
2025-04-22 13:13:55 +0200

    tema 6 1

---
+++ Tema6/src/ejercicio1/PrincipalNReales.java
@@ -0,0 +1,52 @@
+package ejercicio1;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class PrincipalNReales {
+	public static void main(String[] args) {
+		//creamos variable número
+		double num;
+		//creamos variable suma
+		double suma=0;
+		//creamos contador
+		int cont=0;
+		//variable media
+		double media=0;
+// try 		
+		try {
+			//creamos objeto escaner y le pasamos el filereader con la dirección del fichero
+			Scanner sc = new Scanner(new FileReader("src\\ejercicio1\\NumerosReales.txt"));
+			
+			//comprobamos que el fichero tiene números y va avanzando por entre los números
+			while(sc.hasNextDouble()) {
+				//guardamos el número en la variable num
+				num=sc.nextDouble();
+				//sumamos al total el número
+				suma+=num;
+				//incrementamos contador
+				cont++;
+			}
+			//calculamos la media
+			media=suma/cont;
+			
+			//imprimimos suma
+			System.out.println("Suma números: "+suma);
+			//imprimimos media
+			System.out.println("Media números: "+media);
+			
+			//cerramos escaner
+			sc.close();
+			
+			//capturamos la excepción
+		} catch (FileNotFoundException e) {
+			//mensaje de error
+			System.out.println("El fichero no existe");
+		}
+
+	
+		
+		
+	}
+}
