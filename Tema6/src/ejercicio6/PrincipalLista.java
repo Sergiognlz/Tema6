f36ea5c07cce89c19057d4d967b1f351ee434573
Sergio <Sergio@192.168.1.135>
2025-04-22 20:44:07 +0200

    6

---
+++ Tema6/src/ejercicio6/PrincipalLista.java
@@ -0,0 +1,56 @@
+package ejercicio6;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class PrincipalLista {
+	public static void main(String[] args) {
+
+		// variable número
+		int num;
+		// creamos lista
+		List<Integer> listaNumeros = new ArrayList<>();
+
+		// try catch con el scanner y el filereader con la dirección del la lista1.
+		try (Scanner sc = new Scanner(new FileReader("\src\\ejercicio6\\Lista1.txt"))) {
+			// mientras haya números seguirá repitiendo
+			while (sc.hasNext()) {
+				//guardamos el número que leemos en la variable num
+				num = sc.nextInt();
+				//añadimos el número a la lista
+				listaNumeros.add(num);
+			}
+			//ordenamos la lista
+			Collections.sort(listaNumeros);
+			
+			//recogemos la excepción en caso de que no encuentre el fichero
+		} catch (FileNotFoundException e) {
+			//mensaje error
+			System.out.println("Fichero no encontrado");
+			
+			//try catch con el buffer de escritura y la dirección de la lista 2, añadimos el append con el true para que concatene
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter("\src\\ejercicio6\\Lista2.txt", true))) {
+				//for each para recorrer la lista
+				for (int numero : listaNumeros) {
+					//guardamos el número en el fichero
+					bw.write(numero);
+					//añadimos salto de línea
+					bw.newLine();
+				}
+				//capturamos la excepción en caso que no se pueda leer
+			} catch (IOException e1) {
+				//mensaje de error
+				System.out.println("No se puede leer el fichero");
+			}
+
+		}
+
+	}
+}
